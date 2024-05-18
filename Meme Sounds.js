57f29ee009714fae69e38b28a1a450a84e2db719
HuxOP <168302110+HuxOP@users.noreply.github.com>
2024-05-18 19:00:00 +0200

    Add files via upload

---
+++ Meme Sounds.js
@@ -0,0 +1,13 @@
+alert ("Welcome to MemeSounds by HuxOP & CeswarriorYT")
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    var notification = document.getElementById('notification');
+    notification.classList.add('show');
+});
+
+document.getElementById('closeNotification').addEventListener('click', function() {
+    var notification = document.getElementById('notification');
+    notification.classList.remove('show');
+});
+
+
